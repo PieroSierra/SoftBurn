b69d90432a9c77d039ecc42117a5abc564940e2b
Piero Sierra <piero.sierra@outlook.com>
2026-01-04 20:51:13 +0000

    Initial Commit

---
+++ SoftBurn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoftBurn
+//
+//  Created by Piero Sierra on 04/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoftBurn/SoftBurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftBurnApp.swift
+//  SoftBurn
+//
+//  Created by Piero Sierra on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SoftBurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
